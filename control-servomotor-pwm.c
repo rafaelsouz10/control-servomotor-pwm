19462e0289ccf52e4ce26d950af8f3f4b404dd28
Rafael Souza Barbosa <rafaelsouz10@gmail.com>
2025-02-10 11:50:09 -0300

    organização do código

--- control-servomotor-pwm.c
+++ control-servomotor-pwm.c
@@ -11,7 +11,6 @@ uint16_t duty_cycle; //variável para ser salvo a cada ciclo de trabalho utiliza
 void set_servo_angle(uint slice_num) {
     
     for (int i = 0; i < 3; i++){    //roda cada clico do vetor angles
-        angles[i];
         if (angles[i] == 0) {
             // 0 graus -> 500μs, com período de 20ms -> Duty Cycle = 500 / 20000 = 0.025% 
             duty_cycle = 500;  // ciclo de trabalo para 0 graus
@@ -50,19 +49,19 @@ int main() {
     while (true) { 
         // Move de 0 a 180 graus
         while (duty_cycle <= 2400) {
-            pwm_set_chan_level(slice_num, PWM_CHAN_A, duty_cycle); //canal pwm será incrementado +5μs o duty cycle até chegar a 2400μs (180 graus)
             duty_cycle += 5;  // Incrementa o ciclo ativo em 5μs
+            pwm_set_chan_level(slice_num, PWM_CHAN_A, duty_cycle); //canal pwm será incrementado +5μs o duty cycle até chegar a 2400μs (180 graus) 
             sleep_ms(10);     // Pausa de 10ms entre os ajustes
         }
-        printf("Servo movido para 180 graus\n");
+        printf("Servo movido de 0 para 180 graus\n");
 
         // Move de 180 a 0 graus
         while (duty_cycle >= 500) {
-            pwm_set_chan_level(slice_num, PWM_CHAN_A, duty_cycle); //canal pwm será decrementado +5μs o duty cycle até chegar a 500μs (0 graus)
             duty_cycle -= 5;  // Decrementa o ciclo ativo em 5μs
+            pwm_set_chan_level(slice_num, PWM_CHAN_A, duty_cycle); //canal pwm será decrementado +5μs o duty cycle até chegar a 500μs (0 graus)
             sleep_ms(10);     // Pausa de 10ms entre os ajustes
         }
-        printf("Servo movido para 0 graus\n");
+        printf("Servo movido de 180 para 0 graus\n");
     }
 
     return 0;
